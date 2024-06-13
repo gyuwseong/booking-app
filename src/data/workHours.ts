b2119bf3b6687f6bb7fee8fd4c9ce9dc5d55839c
gyuwseong <gw777seong@gmail.com>
2024-06-13 19:38:38 +0900

    chore: add mock data

---
+++ src/data/workHours.ts
@@ -0,0 +1,46 @@
+export const workhours = [
+  {
+    close_interval: 72000,
+    is_day_off: false,
+    open_interval: 36000,
+    weekday: 1,
+  },
+
+  {
+    close_interval: 36900,
+    is_day_off: false,
+    open_interval: 36900,
+    weekday: 2,
+  },
+
+  {
+    close_interval: 72000,
+    is_day_off: false,
+    open_interval: 36000,
+    weekday: 3,
+  },
+  {
+    close_interval: 72000,
+    is_day_off: false,
+    open_interval: 36000,
+    weekday: 4,
+  },
+  {
+    close_interval: 72000,
+    is_day_off: false,
+    open_interval: 36000,
+    weekday: 5,
+  },
+  {
+    close_interval: 72000,
+    is_day_off: false,
+    open_interval: 36000,
+    weekday: 6,
+  },
+  {
+    close_interval: 72000,
+    is_day_off: false,
+    open_interval: 36000,
+    weekday: 7,
+  },
+];
